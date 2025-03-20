b5d1c8bc9e7bac89f908605f181af9662a3151c2
John Zeng <johnzeng878@gmail.com>
2025-03-19 22:19:16 -0400

    number of tests performed in test continuous stochastic

--- tests/test_alchemy/test_continuous_stochastic.py
+++ tests/test_alchemy/test_continuous_stochastic.py
@@ -1,8 +1,9 @@
-import numpy as np
+import pytest
 import scipy.stats as stats
 from src.alchemy.continuous_stochastic_alchemy_env import ContinuousStochasticAlchemyEnv
 
 class TestContinuousStochasticAlchemyEnv():
+    @pytest.mark.timeout(3)
     def test_generate_world(self):
         def truncnorm(mean, std, lower, upper):
             a, b = (lower - mean) / std, (upper - mean) / std
@@ -20,7 +21,8 @@ class TestContinuousStochasticAlchemyEnv():
         for pot in env.world.actions[:-1]:
             assert pot.feature_name == env.features[pot.feature_idx], 'Potion\'s feature id does not match feature name.'
         num_samples = 1000
-        num_tests = 10
+        num_tests = 20
+        acceptable_failures = 3
         for i in range(env.num_features):
             num_successes = 0
             for _ in range(num_tests):
@@ -30,4 +32,4 @@ class TestContinuousStochasticAlchemyEnv():
                 _, p_value = stats.kstest(samples, distributions[i].cdf)
                 if p_value > 0.05:
                     num_successes += 1
-            assert num_successes > 8, f'With p-values over 0.05, the potion that affects "{env.features[i]}" failed the Kolmogorov-Smirnov test {num_tests - num_successes} times out of {num_tests}. Run the test again to see if this is a false positive.'
+            assert num_successes >= num_tests - acceptable_failures, f'With p-values over 0.05, the potion that affects "{env.features[i]}" failed the Kolmogorov-Smirnov test {num_tests - num_successes} times out of {num_tests}. Run the test again to see if this is a false positive.'
